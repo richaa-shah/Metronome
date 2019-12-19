e86dcf1eef90afa36dd365a94d3e80153504df2a
richaa-shah <16167921+richaa-shah@users.noreply.github.com>
2019-12-19 14:27:15 +0000

    Create metronome

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Metronome from './Metronome';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Metronome />, rootElement);
